ed9429892e69ad151b74d14c50460c1d9b853c20
kakalote2008 <lhzwb2008@163.com>
2019-05-06 10:56:37 +0800

    up

--- buildwin.js
+++
@@ -1,9 +0,0 @@
-var electronInstaller = require('electron-winstaller');
-resultPromise = electronInstaller.createWindowsInstaller({
-    appDirectory: 'HupaiHelper-win32-x64',
-    outputDirectory: 'HupaiHelper-installer-win32-x64',
-    authors: 'zark',
-    exe: 'HupaiHelper.exe'
-  });
-
-resultPromise.then(() => console.log("It worked!"), (e) => console.log(`No dice: ${e.message}`));
